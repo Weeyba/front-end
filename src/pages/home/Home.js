7452bb76e5657c6fb697665312922be295c78ce9
didi168 <divineoko2018@gmail.com>
2023-08-08 14:41:22 +0100

    first success commit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+   <main>
+    <div className="firstSection">
+    <div className="firstDiv">
+    <h1>Earn as you Engage</h1>
+    </div>
+    <div secondDiv>
+    
+    </div>
+
+    </div>
+   
+   
+   
+   </main>
   )
 }
