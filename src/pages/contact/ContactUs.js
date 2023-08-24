d64b23559ecd39a6fe5a0cf5cdcdc7ee0ec974aa
didi168 <divineoko2018@gmail.com>
2023-08-24 19:59:30 +0100

    nav menu

--- src/pages/contact/ContactUs.js
+++ src/pages/contact/ContactUs.js
@@ -1,15 +1,14 @@
-import React from "react";
+import React from 'react'
 
-import "../../pages/contact/contact.css";
+
+
+
+import '../../pages/contact/contact.css'
 
 export default function ContactUs() {
   return (
-    <div className="contact" style={{ marginTop: 100 }}>
-      <div>
-        <h1>Contact Us:</h1>
-      </div>
+    <div className="contact" style={{marginTop: 100}}>
 
-<<<<<<< HEAD
 
 
     <div>
@@ -37,42 +36,7 @@ export default function ContactUs() {
           <a className="link" href="https://t.me/weeyba_official"><i class="fa-brands fa-telegram"></i></a>
           </div>
         
-=======
-      <div className="con">
-        <div>
-          <p>
-            If you have questions or need any general information, please reach
-            out to us through one of our social channels
-          </p>
-        </div>
-        <hr />
-
-        <div className="firstCon">
-          <h2>For Enquiries:</h2>
-          <a className="link" href="https://wa.me/message/CKAN5VOTKC76I1">
-            <i className="fa-brands fa-whatsapp"></i>
-          </a>
-          <a className="link" href="contact@weeyba.com">
-            <i className="fa-regular fa-envelope"></i>
-          </a>
-          <a
-            className="link"
-            href="https://instagram.com/weeyba_official?igshid=NjIwNzIyMDk2Mg=="
-          >
-            <i className="fa-brands fa-instagram"></i>
-          </a>
-          <a
-            className="link"
-            href="https://www.tiktok.com/@weeyba_official?_t=8f3TkzmUyJC&_r=1"
-          >
-            <i className="fa-brands fa-tiktok"></i>
-          </a>
-          <a className="link" href="https://t.me/weeyba_official">
-            <i className="fa-brands fa-telegram"></i>
-          </a>
-        </div>
-      </div>
->>>>>>> 7da74b889c94aec3064311ae13d98d6c7c0e1028
     </div>
-  );
+    </div>
+  )
 }
